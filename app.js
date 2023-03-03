68f3a5c705a8e202d9eebc88c337bce9231e171d
ds <swaby@protonmail.com>
2023-03-03 16:01:07 -0500

    make receipts in-memory storage object in entry file, import crypto library from node and use it to generate unique ids in receipts/process post route

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const crypto = require("crypto");
 var bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+const receipts = {};
+
 app.get("/", (req, res) => res.send("hello"));
 
+app.get("/receipts/:id/points", (req, res) => {
+  let receipt = receipts[req.params.id];
+  res.json(receipt);
+});
+
 app.post("/receipts/process", (req, res) => {
-  console.log(req.body);
+  let newId = crypto.randomUUID();
+  receipts[newId] = req.body;
+  console.log(receipts);
+  res.json({ id: newId });
 });
 
 app.listen(3000, () => {
