c91fe0c8b2b8331facab900f3502dd847cd9ea98
ds <swaby@protonmail.com>
2023-03-03 19:29:45 -0500

    build receipt validation function to validate receipts being posted

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const crypto = require("crypto");
 const bodyParser = require("body-parser");
 const calcPoints = require("./calcPoints");
+const validateReceipt = require("./validateReceipt");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,10 +14,11 @@ app.get("/", (req, res) => res.send("Receipt-Processor-Challenge"));
 
 // for getting points of a particular receipt
 app.get("/receipts/:id/points", (req, res) => {
-  if(!(req.params.id in receipts)){
+  // if we can't find that receipt respond with status 404 and a message
+  if (!(req.params.id in receipts)) {
     res.status(404);
     res.json({
-      message: "Receipt not found."
+      message: "No receipt found for that id",
     });
   }
   let receipt = receipts[req.params.id];
@@ -27,8 +29,15 @@ app.get("/receipts/:id/points", (req, res) => {
 // for posting new receipts
 app.post("/receipts/process", (req, res) => {
   let newId = crypto.randomUUID();
-  receipts[newId] = req.body;
-  res.json({ id: newId });
+  if (validateReceipt(req.body)) {
+    receipts[newId] = req.body;
+    res.json({ id: newId });
+  } else {
+    res.status(400);
+    res.json({
+      message: "The receipt is invalid",
+    });
+  }
 });
 
 app.listen(3000, () => {
---
+++ validateReceipt.js
@@ -0,0 +1,24 @@
+// This module provides a superficial validation for new receipts being posted
+// It checks to make sure all of the required fields are present in the receipt
+// If there are items in the items array (which there should be or else nothing was actually bought), it will make sure the required fields are present
+// Using persistent storage we could build these kinds of validations into a schema definition 
+
+const validateReceipt = (receipt) => {
+  let fields = ["retailer", "purchaseDate", "purchaseTime", "items", "total"];
+  let itemFields = ["shortDescription", "price"];
+  for (let field of fields) {
+    if (!(field in receipt)) return false;
+  }
+  if (receipt.items.length > 0) {
+    for (let item of receipt.items) {
+      for (let itemField of itemFields) {
+        if (!(itemField in item)) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+module.exports = validateReceipt;
